43ce68e832f81634b665949651336ba1896ba53b
KathiresanTechnoKryon <kathiresan@kryonknowledgeworks.com>
2025-02-07 15:46:02 +0530

    #41 adding missing tags

---
+++ src/main/java/com/kryonknowledgeworks/jats2html/elements/DateInCitation.java
@@ -0,0 +1,90 @@
+package com.kryonknowledgeworks.jats2html.elements;
+
+import com.kryonknowledgeworks.jats2html.Tag;
+import com.kryonknowledgeworks.jats2html.mapbuilder.MetaDataBuilder;
+import com.kryonknowledgeworks.jats2html.util.ClassNameSingleTon;
+import com.kryonknowledgeworks.jats2html.util.Util;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateInCitation implements Tag {
+
+    public static Boolean IMPLEMENT = true;
+
+    public static String ELEMENT_FULL = "<date-in-citation>";
+    public static String ELEMENT= "date-in-citation";
+
+    Node node = null;
+    NodeList childNodes = null;
+    List<Node> nodeList = new ArrayList<>();
+    String html = "";
+
+    public DateInCitation(Node node, MetaDataBuilder metaDataBuilder) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        this.node = node;
+        elementFilter();
+
+        List<String> tagNames = ClassNameSingleTon.getInstance().tagNames;
+
+        for (Node node1 : nodeList) {
+
+            if (tagNames.contains(node1.getNodeName())) {
+
+                String className = ClassNameSingleTon.tagToClassName(node1.getNodeName());
+                if (Boolean.TRUE.equals(ClassNameSingleTon.isImplement(className))) {
+
+                    Object instanceFromClassName = ClassNameSingleTon.createInstanceFromClassName(className, node1, metaDataBuilder);
+                    this.html += ClassNameSingleTon.invokeMethod(instanceFromClassName, "element");
+                }
+            } else if (!node1.getNodeName().equals("#text")){
+
+                this.html += "<pre style='color:red'>'''" + Util.convertToString(node1).replace("<", "&lt;").replace(">", "&gt;") + "'''</pre>";
+            }
+
+        }
+
+
+        if (node.getParentNode().getNodeName().equals("element-citation")){
+            this.html += "("+Util.getHtmlEscapeData(node.getTextContent())+")";
+        }else{
+            this.html += Util.getHtmlEscapeData(node.getTextContent());
+        }
+
+
+
+    }
+
+    @Override
+    public String element() {
+        return html;
+    }
+
+    @Override
+    public List<String> elements() {
+        return null;
+    }
+
+    @Override
+    public Boolean isChildAvailable() {
+        return null;
+    }
+
+    public List<Node> elementFilter() {
+
+        this.childNodes = this.node.getChildNodes();
+
+        for (int i = 0; i < this.childNodes.getLength(); i++) {
+
+            Node firstChild = this.childNodes.item(i);
+
+            if (firstChild != null && !firstChild.getNodeName().equals("#text")) {
+                nodeList.add(firstChild);
+            }
+
+        }
+        return nodeList;
+    }
+}
